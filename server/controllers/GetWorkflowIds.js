ec5bf2d1e480dabd1061bb5b3b44fd36c80271fd
nidhisharma63 <nidhisharma639593@gmail.com>
2024-04-26 09:09:41 +0530

    workflows send according to userId

--- server/controllers/GetWorkflowIds.js
+++ server/controllers/GetWorkflowIds.js
@@ -1,7 +1,9 @@
 import WorkFlow from "../schema/WorkFlowSchema.js";
 const getWorkflowIds = async (req, res, next) => {
   try {
-    const findWorkFlowIds = await WorkFlow.find();
+    // get userId from query
+    const { userId } = req.query;
+    const findWorkFlowIds = await WorkFlow.find({ userId });
     const workFlowIds = findWorkFlowIds.map((workflow) => workflow.workFlowId);
     res.send(workFlowIds);
   } catch (error) {
