06dc84a5cd45fc59cd02dac1831b152b947b80e4
nidhisharma63 <nidhisharma639593@gmail.com>
2024-04-29 08:29:58 +0530

    sidebar functionality added

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,23 +1,107 @@
 import React from "react";
+import { Handle, Position } from "reactflow";
 
-export default () => {
+const FilterDataComponent = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+  return (
+    <>
+      <div className="dndnode Filter Data" onDragStart={(event) => onDragStart(event, "Filter Data")} draggable>
+        Filter Data
+      </div>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+    </>
+  );
+};
+
+const SendPostRequestComponent = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+  return (
+    <>
+      <div
+        className="dndnode Send Post Request"
+        onDragStart={(event) => onDragStart(event, "Send Post Request")}
+        draggable>
+        Send Post Request
+      </div>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+    </>
+  );
+};
+
+const ConvertFormatComponent = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+  return (
+    <>
+      <div className="dndnode Convert Format" onDragStart={(event) => onDragStart(event, "Convert Format")} draggable>
+        Convert Format
+      </div>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+    </>
+  );
+};
+
+const WaitComponent = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+  return (
+    <>
+      <div className="dndnode Wait" onDragStart={(event) => onDragStart(event, "Wait")} draggable>
+        Wait
+      </div>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+    </>
+  );
+};
+
+const Sidebar = ({ edges, nodes }) => {
   const onDragStart = (event, nodeType) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
   };
 
+  console.log({ nodes });
   return (
     <aside>
       <div className="description">You can drag these nodes to the pane on the right.</div>
-      <div className="dndnode input" onDragStart={(event) => onDragStart(event, "input")} draggable>
-        Input Node
+      <div
+        className="dndnode start"
+        onDragStart={(event) => onDragStart(event, "input")}
+        draggable={nodes.some((node) => node.type === "input") ? false : true}
+        style={{
+          opacity: nodes.some((node) => node.type === "input") ? ".2" : "1",
+        }}>
+        Start
       </div>
-      <div className="dndnode" onDragStart={(event) => onDragStart(event, "default")} draggable>
-        Default Node
-      </div>
-      <div className="dndnode output" onDragStart={(event) => onDragStart(event, "output")} draggable>
-        Output Node
+      <FilterDataComponent nodes={nodes} />
+      <SendPostRequestComponent nodes={nodes} />
+      {/* <ConvertFormatComponent nodes={nodes} />
+      <WaitComponent nodes={nodes} /> */}
+      <div
+        className="dndnode output"
+        onDragStart={(event) => onDragStart(event, "output")}
+        draggable={nodes.some((node) => node.type === "output") ? false : true}
+        style={{
+          opacity: nodes.some((node) => node.type === "output") ? ".2" : "1",
+        }}>
+        End
       </div>
     </aside>
   );
 };
+
+export { ConvertFormatComponent, FilterDataComponent, SendPostRequestComponent, Sidebar, WaitComponent };
--- client/src/pages/ReactFlow.jsx
+++ client/src/pages/ReactFlow.jsx
@@ -1,31 +1,77 @@
-import React, { useCallback, useRef, useState } from "react";
-import ReactFlow, { Controls, ReactFlowProvider, addEdge, useEdgesState, useNodesState } from "reactflow";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { toast } from "react-toastify";
+import ReactFlow, {
+  Controls,
+  ReactFlowProvider,
+  addEdge,
+  applyEdgeChanges,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
 import "reactflow/dist/style.css";
+import { useCreateWorkflow } from "src/hooks/useWorkflow";
+import { v4 as uuidv4 } from "uuid";
+import { FilterDataComponent, SendPostRequestComponent, Sidebar } from "../component/Sidebar";
 
-import Sidebar from "../component/Sidebar";
-
-// import "./index.css";
-
-const initialNodes = [
-  {
-    id: "1",
-    type: "input",
-    data: { label: "input node" },
-    position: { x: 250, y: 5 },
-  },
-];
+let nodeType = {
+  "Filter Data": FilterDataComponent,
+  "Send Post Request": SendPostRequestComponent,
+  // Wait: WaitComponent,
+  // "Convert Format": ConvertFormatComponent,
+};
+const initialNodes = [];
 
 let id = 0;
-const getId = () => `dndnode_${id++}`;
+const getId = () => `${id++}`;
 
 const DnDFlow = () => {
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [edges, setEdges] = useEdgesState([]);
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const { mutateAsync, error } = useCreateWorkflow();
+  const [isLoading, setIsLoading] = useState(false);
+  const id = useMemo(() => uuidv4(), []);
+  const onConnect = useCallback(
+    (params) => {
+      // const nodeIsAlreadyConnected = edges.some((edge) => edge.target === params.source);
+      // console.log(nodeIsAlreadyConnected, "nodeIsAlreadyConnected", edges, params);
+      // if (nodeIsAlreadyConnected) {
+      //   toast.error("You have connected this node already");
+      //   return;
+      // }
+      const invalidConnection = edges.some((edge) => {
+        return edge.source === params.target && edge.target === params.source;
+      });
+      if (invalidConnection) {
+        toast.error("Invalid Connection");
+        return;
+      }
+      // Check that source and target nodes are not the same (Rule 1)
+      if (params.source === params.target) {
+        toast.error("Cannot connect a node to itself.");
+        return;
+      }
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+      // Check if the source node already has an outgoing connection (Rule 2)
+      const sourceHasOutgoingConnection = edges.some((edge) => edge.source === params.source);
+      if (sourceHasOutgoingConnection) {
+        toast.error("This node already has an outgoing connection.");
+        return;
+      }
 
+      // Check if the target node already has an incoming connection (Rule 3)
+      const targetHasIncomingConnection = edges.some((edge) => edge.target === params.target);
+      if (targetHasIncomingConnection) {
+        toast.error("This node already has an incoming connection.");
+        return;
+      }
+
+      // Add the edge if all checks pass
+      setEdges((eds) => addEdge(params, eds));
+    },
+    [edges, setEdges]
+  );
   const onDragOver = useCallback((event) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = "move";
@@ -50,12 +96,78 @@ const DnDFlow = () => {
         id: getId(),
         type,
         position,
-        data: { label: `${type} node` },
+        data: { label: `${type === "input" ? "Start" : type === "output" ? "End" : type}` },
       };
 
       setNodes((nds) => nds.concat(newNode));
     },
     [reactFlowInstance]
+  );
+
+  const saveWorkFlow = useCallback(() => {
+    if (reactFlowInstance) {
+      const flow = {
+        nodes: reactFlowInstance.getNodes(),
+        edges: reactFlowInstance.getEdges(),
+      };
+      // console.log(flow);
+
+      if (flow.edges.length < flow.nodes.length - 1) {
+        toast.error("Please add All nodes in the flow");
+        return;
+      }
+
+      const isEndIncludedInNode = flow.nodes.some((node) => node.data.label === "End");
+
+      if (!isEndIncludedInNode) {
+        toast.error("Please add End node in the flow");
+        return;
+      }
+      const getAllSourceValues = flow.edges.map((edge) => edge.source);
+      console.log({ getAllSourceValues }, flow.edges, flow.nodes);
+      const extractedValues = getAllSourceValues.map((index) => {
+        const indexNumber = +index;
+        // console.log({ indexNumber });
+        return flow.nodes[indexNumber].data.label;
+      });
+
+      // console.log({ extractedValues });
+
+      // console.log({ extractedValues });
+      if (extractedValues[0] !== "Start") {
+        toast.error("First node must be Start");
+        return;
+      }
+
+      if (extractedValues.includes("End")) {
+        toast.error("Last node must be End");
+        return;
+      }
+
+      // setIsLoading(true);
+      mutateAsync({
+        workFlowSequence: [...extractedValues, "End"],
+        workFlowId: id,
+      }).then(() => {
+        setIsLoading(false);
+      });
+    }
+  }, [nodes, edges, id]);
+
+  const onEdgesChange = useCallback((changes) => {
+    setEdges((eds) => applyEdgeChanges(changes, eds));
+  }, []);
+
+  // Callback to remove an edge
+  const onEdgeClick = useCallback(
+    (event, edge) => {
+      // Prevents the default text selection behavior
+      event.preventDefault();
+
+      // Here we filter out the clicked edge from the edges array to remove it
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+    },
+    [setEdges]
   );
 
   return (
@@ -66,17 +178,20 @@ const DnDFlow = () => {
             nodes={nodes}
             edges={edges}
             onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
             onConnect={onConnect}
             onInit={setReactFlowInstance}
             onDrop={onDrop}
             onDragOver={onDragOver}
+            onEdgeClick={onEdgeClick}
+            onEdgesChange={onEdgesChange}
+            nodeTypes={nodeType}
             fitView>
             <Controls />
           </ReactFlow>
         </div>
-        <Sidebar />
+        <Sidebar edges={edges} nodes={nodes} />
       </ReactFlowProvider>
+      <button onClick={saveWorkFlow}>Save</button>
     </div>
   );
 };
